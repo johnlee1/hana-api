68881a290f15dee0072b3f8c8bb47012ea8318fe
johnlee1 <telljlee@gmail.com>
2016-12-27 23:32:30 -0500

    add password change

--- config/database.js
+++ config/database.js
@@ -6,8 +6,7 @@ const Mongoose = require('mongoose');
 
 //const parameters = require('./parameters.json');
 //Mongoose.connect('mongodb://' + parameters.database.host + '/' + parameters.database.db);
-//Mongoose.connect(process.env.DB_CONNECTION)
-Mongoose.connect("mongodb://prayforhana:kjlraylicc!!1@ds031835.mlab.com:31835/prayforhana");
+Mongoose.connect(process.env.DB_CONNECTION)
 
 const db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error'));
